67a79b65b384581119b9bc316834634e8403732b
tomlebl <tl12@st-andrews.ac.uk>
2020-06-12 23:20:31 +0100

    App.js refactored

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, Suspense } from 'react'
+import React, { useState, Suspense } from 'react'
 import { Route, Switch, Redirect, withRouter } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { closeAuthModal, signInHandler, signOutHandler, closeDashDrawer } from './store/actions'
@@ -21,91 +21,90 @@ import StatusDrawer from './components/StatusDrawer/StatusDrawer'
 
 const { Header, Sider, Content, Footer } = Layout
 
-export class App extends Component {
-  state = {
-    adminMenuCollapsed: true
+const App = props => {
+  const [adminMenuCollapsed, setAdminMenuCollapsed] = useState(true)
+
+  const toggleAdminMenu = () => {
+    setAdminMenuCollapsed(!adminMenuCollapsed)
   }
 
-  toggleAdminMenu = () => {
-    this.setState(prevState => {
-      return { adminMenuCollapsed: !prevState.adminMenuCollapsed }
-    })
-  }
+  // const closeDrawerHandler = () => {
+  //   const newDrawerStatus = { ...this.state.drawerStatus }
+  //   newDrawerStatus.visible = false
+  //   newDrawerStatus.tableData = []
+  // }
 
-  closeDrawerHandler = () => {
-    const newDrawerStatus = { ...this.state.drawerStatus }
-    newDrawerStatus.visible = false
-    newDrawerStatus.tableData = []
-    this.setState({ drawerStatus: newDrawerStatus })
-  }
+  const {
+    user,
+    adminAccess,
+    authModalVisible,
+    closeModal,
+    onSignIn,
+    onSignOut,
+    onCloseDrawer,
+    drawerStatus
+  } = props
 
-  render() {
-    const { adminMenuCollapsed } = this.state
-    const { user, adminAccess, authModalVisible, closeModal, onSignIn, onSignOut } = this.props
+  // Lazy loading - TODO: add to other container imports to improve performance once app gets bigger
+  const Users = React.lazy(() => import('./containers/Users/Users'))
 
-    // Lazy loading - TODO: add to other container imports to improve performance once app gets bigger
-    const Users = React.lazy(() => import('./containers/Users/Users'))
-
-    //Logic for authentication modal. Different modal is rendered depending whether a user is logged in or not
-    let authModal = null
-    if (authModalVisible) {
-      if (user) {
-        authModal = (
-          <LogoutModal visible={authModalVisible} cancelClicked={closeModal} okClicked={onSignOut} />
-        )
-      } else {
-        authModal = (
-          <LoginModal visible={authModalVisible} cancelClicked={closeModal} signInClicked={onSignIn} />
-        )
-      }
+  //Logic for authentication modal. Different modal is rendered depending whether a user is logged in or not
+  let authModal = null
+  if (authModalVisible) {
+    if (user) {
+      authModal = <LogoutModal visible={authModalVisible} cancelClicked={closeModal} okClicked={onSignOut} />
+    } else {
+      authModal = (
+        <LoginModal visible={authModalVisible} cancelClicked={closeModal} signInClicked={onSignIn} />
+      )
     }
-
-    return (
-      <Layout>
-        {adminAccess ? (
-          <Affix className={classes.AdminMenu}>
-            <Sider trigger={null} className={classes.Sider} collapsible collapsed={adminMenuCollapsed}>
-              <AdminMenu collapsed={adminMenuCollapsed} />
-            </Sider>
-          </Affix>
-        ) : null}
-
-        <Layout>
-          <Affix>
-            <Header className={classes.Header}>
-              <NavBar collapsed={adminMenuCollapsed} toggleClicked={this.toggleAdminMenu} />
-            </Header>
-          </Affix>
-          <Content className={classes.Content}>
-            <Suspense fallback={<Spin size='large' tip='Loading ...' style={{ margin: '200px' }} />}>
-              {!user && <Redirect to='/dashboard' />}
-              <Switch>
-                <Route
-                  path='/dashboard/users'
-                  render={() => {
-                    return adminAccess ? <Users /> : <Error403 />
-                  }}
-                />
-                <Route path='/dashboard/groups' component={adminAccess ? Groups : Error403} />
-                <Route path='/dashboard/instruments' component={adminAccess ? Instruments : Error403} />
-                <Route path='/dashboard/experiments' component={adminAccess ? Experiments : Error403} />
-                <Route exact path='/dashboard' render={() => <Dashboard />} />
-                <Redirect from='/dashboard/dashboard' to='/dashboard' />
-                <Redirect exact from='/' to='/dashboard' />
-                <Route component={Error404} />
-              </Switch>
-            </Suspense>
-            {authModal}
-            <StatusDrawer status={this.props.drawerStatus} closeClicked={this.props.onCloseDrawer} />
-            <BackTop visibilityHeight={200} />
-          </Content>
-          <Footer className={classes.Footer}>
-            <Credits />
-          </Footer>
-        </Layout>
-      </Layout>
-    )
   }
+
+  return (
+    <Layout>
+      {adminAccess ? (
+        <Affix className={classes.AdminMenu}>
+          <Sider trigger={null} className={classes.Sider} collapsible collapsed={adminMenuCollapsed}>
+            <AdminMenu collapsed={adminMenuCollapsed} />
+          </Sider>
+        </Affix>
+      ) : null}
+
+      <Layout>
+        <Affix>
+          <Header className={classes.Header}>
+            <NavBar collapsed={adminMenuCollapsed} toggleClicked={toggleAdminMenu} />
+          </Header>
+        </Affix>
+        <Content className={classes.Content}>
+          <Suspense fallback={<Spin size='large' tip='Loading ...' style={{ margin: '200px' }} />}>
+            {!user && <Redirect to='/dashboard' />}
+            <Switch>
+              <Route
+                path='/dashboard/users'
+                render={() => {
+                  return adminAccess ? <Users /> : <Error403 />
+                }}
+              />
+              <Route path='/dashboard/groups' component={adminAccess ? Groups : Error403} />
+              <Route path='/dashboard/instruments' component={adminAccess ? Instruments : Error403} />
+              <Route path='/dashboard/experiments' component={adminAccess ? Experiments : Error403} />
+              <Route exact path='/dashboard' render={() => <Dashboard />} />
+              <Redirect from='/dashboard/dashboard' to='/dashboard' />
+              <Redirect exact from='/' to='/dashboard' />
+              <Route component={Error404} />
+            </Switch>
+          </Suspense>
+          {authModal}
+          <StatusDrawer status={drawerStatus} closeClicked={onCloseDrawer} />
+          <BackTop visibilityHeight={200} />
+        </Content>
+        <Footer className={classes.Footer}>
+          <Credits />
+        </Footer>
+      </Layout>
+    </Layout>
+  )
 }
 
 const mapStateToProps = state => {
