6f897a9abdf579b2ba2bd4f15dfd3cad01344171
tomlebl <tl12@st-andrews.ac.uk>
2022-01-14 12:57:56 +0000

    adding github link

--- src/components/Credits/Credits.js
+++ src/components/Credits/Credits.js
@@ -7,41 +7,46 @@ import nodeLogo from '../../assets/node-logo.svg'
 import mongoLogo from '../../assets/mongoDB-logo.png'
 import socketIO from '../../assets/websocket.png'
 import nginxLogo from '../../assets/nginx.png'
+import gitHubLogo from '../../assets/GitHub-logo.png'
 import classes from './Credits.module.css'
 
 const credits = () => {
-	return (
-		<div className={classes.Credits}>
-			<span className={classes.Text}>Powered by</span>
-			<a href='https://reactjs.org/'>
-				<img src={reactLogo} alt='React Logo' />
-			</a>
-			<Divider type='vertical' />
-			<a href='https://ant.design/'>
-				<img src={antdLogo} alt='Ant Design Logo' />
-			</a>
-			<Divider type='vertical' />
-			<a href='https://redux.js.org/'>
-				<img src={reduxLogo} alt='Redux Logo' />
-			</a>
-			<Divider type='vertical' />
-			<a href='https://nodejs.org/'>
-				<img src={nodeLogo} alt='Node.js Logo' />
-			</a>
-			<Divider type='vertical' />
-			<a href='https://socket.io/'>
-				<img src={socketIO} alt='Socket.io Logo' />
-			</a>
-			<Divider type='vertical' />
-			<a href='https://www.mongodb.com/'>
-				<img src={mongoLogo} alt='Node.js Logo' />
-			</a>
-			<Divider type='vertical' />
-			<a href='https://www.nginx.com/'>
-				<img src={nginxLogo} alt='Nginx Logo' />
-			</a>
-		</div>
-	)
+  return (
+    <div className={classes.Credits}>
+      <span className={classes.Text}>Powered by</span>
+      <a href='https://reactjs.org/'>
+        <img src={reactLogo} alt='React Logo' />
+      </a>
+      <Divider type='vertical' />
+      <a href='https://ant.design/'>
+        <img src={antdLogo} alt='Ant Design Logo' />
+      </a>
+      <Divider type='vertical' />
+      <a href='https://redux.js.org/'>
+        <img src={reduxLogo} alt='Redux Logo' />
+      </a>
+      <Divider type='vertical' />
+      <a href='https://nodejs.org/'>
+        <img src={nodeLogo} alt='Node.js Logo' />
+      </a>
+      <Divider type='vertical' />
+      <a href='https://socket.io/'>
+        <img src={socketIO} alt='Socket.io Logo' />
+      </a>
+      <Divider type='vertical' />
+      <a href='https://www.mongodb.com/'>
+        <img src={mongoLogo} alt='Node.js Logo' />
+      </a>
+      <Divider type='vertical' />
+      <a href='https://www.nginx.com/'>
+        <img src={nginxLogo} alt='Nginx Logo' />
+      </a>
+      <Divider type='vertical' />
+      <a href='https://github.com/nomad-nmr'>
+        <img src={gitHubLogo} alt='GitHub Logo' />
+      </a>
+    </div>
+  )
 }
 
 export default credits
